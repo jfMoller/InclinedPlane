e1faa92c5c149068bddbab53697d84b59052dd49
Johan <johan.moller040@gmail.com>
2022-12-21 12:04:06 +0100

    working on box-slide ratios

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-import { box, plane } from "./nodes.js";
+import { root, box, plane } from "./nodes.js";
 import { handlesKeyDown, handlesKeyUp } from "./event.js";
 
 let planeHeight = 39;
@@ -7,6 +7,8 @@ let planeHypotenuse;
 let planeAngle;
 let boxBottom = -2;
 let boxRight = 2;
+let boxSlideBottom = -120;
+let boxSlideRight = 2350;
 
 let mass = 10; //kg
 let mg = 9.82 * mass;
@@ -19,13 +21,20 @@ export let key = { up: false, down: false };
 addEventListener("keydown", handlesKeyDown);
 addEventListener("keyup", handlesKeyUp);
 
+box.addEventListener("click", () => {
+  box.setAttribute("class", "box-animation");
+  setTimeout(() => {
+    box.removeAttribute("class", "box-animation");
+  }, 10000);
+});
+
 function render() {
   planeHypotenuse = Math.sqrt(
     Math.pow(planeHeight, 2) + Math.pow(planeWidth, 2)
   );
   planeAngle = Math.asin(planeHeight / planeHypotenuse);
-  console.log(planeHeight);
-
+/*   console.log(window.getComputedStyle(root).getPropertyValue("--box_slide_bottom"));
+ */
   maintainsGraphicalPosition();
   requestAnimationFrame(render);
 }
@@ -37,6 +46,9 @@ function maintainsGraphicalPosition() {
       planeHeight += 5;
       boxBottom -= 0.3;
       boxRight += 0.16;
+      boxSlideBottom += -Math.exp(2.776);
+      boxSlideRight += Math.exp(0.77);
+      
     } else {
       return;
     }
@@ -45,6 +57,8 @@ function maintainsGraphicalPosition() {
       planeHeight -= 5;
       boxBottom -= -0.3;
       boxRight += -0.16;
+      boxSlideBottom += Math.exp(2.776);
+      boxSlideRight += -Math.exp(0.77);
     } else {
       return;
     }
@@ -53,4 +67,10 @@ function maintainsGraphicalPosition() {
   box.style.transform = "rotate(" + -planeAngle.toString() + "rad)";
   box.style.bottom = boxBottom.toString() + "px";
   box.style.right = boxRight.toString() + "px";
+
+  root.style.setProperty("--box_bottom", boxBottom + "px");
+  root.style.setProperty("--box_right", boxRight + "px");
+
+  root.style.setProperty("--box_slide_bottom", boxSlideBottom + "px");
+  root.style.setProperty("--box_slide_right", boxSlideRight + "px");
 }
--- nodes.js
+++ nodes.js
@@ -1,2 +1,3 @@
+export const root = document.documentElement;
 export let box = document.querySelector("#box");
 export let plane = document.querySelector("#plane");
