251f6180a008e221c8307bb6fbd287be4d35dcd6
Johan <johan.moller040@gmail.com>
2022-12-21 07:58:59 +0100

    graphical position ratios set, testing sliding animations

--- main.js
+++ main.js
@@ -1,49 +1,56 @@
 import { box, plane } from "./nodes.js";
 import { handlesKeyDown, handlesKeyUp } from "./event.js";
 
-let planeHeight = 200;
+let planeHeight = 39;
 let planeWidth = 800; //px
 let planeHypotenuse;
 let planeAngle;
-let boxBottom = 80;
-let boxRight = 100;
+let boxBottom = -2;
+let boxRight = 2;
 
-export let key = {up: false, down: false};
+let mass = 10; //kg
+let mg = 9.82 * mass;
+let normalForce;
+let F1;
+let frictionForce = F1;
+let frictionNumber = F1 / normalForce;
+
+export let key = { up: false, down: false };
 addEventListener("keydown", handlesKeyDown);
 addEventListener("keyup", handlesKeyUp);
 
 function render() {
-    planeHypotenuse = Math.sqrt(Math.pow(planeHeight, 2) + Math.pow(planeWidth, 2));
-    planeAngle = Math.asin((planeHeight / planeHypotenuse));
-    console.log(planeHeight)
+  planeHypotenuse = Math.sqrt(
+    Math.pow(planeHeight, 2) + Math.pow(planeWidth, 2)
+  );
+  planeAngle = Math.asin(planeHeight / planeHypotenuse);
+  console.log(planeHeight);
 
-
-
-  maintainsPositionalRatio();
+  maintainsGraphicalPosition();
   requestAnimationFrame(render);
 }
 render();
 
-function maintainsPositionalRatio() {
+function maintainsGraphicalPosition() {
   if (key.up) {
-    planeHeight += 1;
-    boxBottom += 0.5;
-    boxRight += -0.5;
-  }
-  if (key.down) {
-    if (planeHeight >= 0) {
-    planeHeight -= 1;
-   
-    boxBottom -= -0.5;
-    boxRight -= -0.5;
-}
-    else {return;}
-
+    if (planeHeight <= 500) {
+      planeHeight += 5;
+      boxBottom -= 0.3;
+      boxRight += 0.16;
+    } else {
+      return;
+    }
+  } else if (key.down) {
+    if (planeHeight >= 40) {
+      planeHeight -= 5;
+      boxBottom -= -0.3;
+      boxRight += -0.16;
+    } else {
+      return;
+    }
   }
   plane.style.borderBottomWidth = planeHeight.toString() + "px";
   box.style.transform = "rotate(" + -planeAngle.toString() + "rad)";
-box.style.bottom = boxBottom.toString() + "px";
-box.style.right = boxRight.toString() + "px";
-
+  box.style.bottom = boxBottom.toString() + "px";
+  box.style.right = boxRight.toString() + "px";
 }
-
